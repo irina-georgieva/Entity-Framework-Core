4840ba3ae4b68431d7ab48800ead858efa6d4fca
Irina Georgieva <georgieva2490@gmail.com>
2022-07-14 23:10:31 +0200

    Added new solution

---
+++ 10 Entity Relations Exercise/03 Football Betting/P03_FootballBetting.Data/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public static class Config
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-B86AG49\SQLEXPRESS;Database=FootballBetting;Integrated Security=True;";
+
+    }
+}
