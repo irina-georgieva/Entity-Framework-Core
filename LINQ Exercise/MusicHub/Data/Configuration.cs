ec248290f1ab7ba2db8373d73285169b4b0cb5fe
Irina Georgieva <90099765+irina-georgieva@users.noreply.github.com>
2022-07-21 23:28:04 +0200

    Delete LINQ Exercise directory

--- LINQ Exercise/MusicHub/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-B86AG49\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
-    }
-}
