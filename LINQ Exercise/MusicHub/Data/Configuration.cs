0c218f374b5c8ec80995d9e9afef17e1e468229c
Irina Georgieva <georgieva2490@gmail.com>
2022-07-21 23:11:59 +0200

    Added new solutions

---
+++ LINQ Exercise/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-B86AG49\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+    }
+}
