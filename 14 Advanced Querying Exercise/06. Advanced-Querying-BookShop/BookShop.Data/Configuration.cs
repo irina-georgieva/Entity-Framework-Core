3935792be112f679a5e5e9b6a2e713d8d9f3c5f1
Irina Georgieva <georgieva2490@gmail.com>
2022-07-29 23:39:44 +0200

    Added new solutions

---
+++ 14 Advanced Querying Exercise/06. Advanced-Querying-BookShop/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => @"Server=DESKTOP-B86AG49\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
