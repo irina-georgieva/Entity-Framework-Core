31601d107fd9c10d969f2e23ff835deddce0a310
Irina Georgieva <georgieva2490@gmail.com>
2022-08-07 12:29:28 +0300

    Added new solutions

---
+++ Regular Exam 06 August 2022/01. Model Definition_Skeleton/Footballers/DataProcessor/ExportDto/ExportCoachXmlAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Coach")]
+    public class ExportCoachXmlAttribute
+    {
+        [XmlAttribute("FootballersCount")]
+        public int FootballersCount { get; set; }
+    }
+}
